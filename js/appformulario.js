d1f66b8789e91a4bf196265cbfae63da2a68d2b9
Chriztian2000 <cabetancourt@uce.edu.ec>
2024-12-11 17:22:57 -0500

    tarea7

---
+++ js/appformulario.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            persona: {
+                nombre: '',
+                apellido: '',
+                hobbie: '',
+                fechaNacimiento: ''
+            },
+            listaPersonas: []
+        }
+    },
+    methods: {
+        agregarPersona() {
+            const nuevaPersona = { ...this.persona };
+            this.listaPersonas.push(nuevaPersona);
+            this.persona.nombre = '';
+            this.persona.apellido = '';
+            this.persona.hobbie = '';
+            this.persona.fechaNacimiento = '';
+        }
+    }
+});
+
+app.mount('#myApp');
